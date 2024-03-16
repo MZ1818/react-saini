1da530d16171c3c2fb477a702f8cd74c9d2c492a
MZ1818 <ahmadmufazzal18@gmail.com>
2024-03-16 17:32:54 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,36 +1,55 @@
-//the 1st intake is the element(tags), 2nd intake takes the attributes(any num of attributes can be passed), 3rd intake is what we want to pass/print/child
-//the 2nd & 3rd intake are props
-//this heading i.e createElement will return an JS Object.
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "hello from REACT update automattically by parcel"
+//React.CreateElement returns => Object (React Element) => when it is rendered on browser then chnges to => HTMLElecment(render)
+
+//we will not be using older method to create React Element
+//Rather we will be using JSX like this
+
+//JSX when using in 1 line==we dont need to put circular brackets
+//when we write JSX code in more than 1 line we have use ( ),
+
+//React Element
+const jsxHeading = (
+  <h1 id="heading" className="header">
+    React using JSX
+  </h1>
 );
+
+//React functional componenrt
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      <h1 className="heading">Namaskar Sahab</h1>
+    </div>
+  );
+};
+
+const num = 111111111111;
+
+//rendering react component inside another react component
+//passing REACT ELEMENT inside COMPONENT
+const HeadingComponent2 = () => {
+  return (
+    <div className="contain">
+      {/* {num} */}
+      {/* {100 + 200} */}
+      {jsxHeading}
+
+      {/* all 3 ways below of rendering React components are same */}
+
+      <HeadingComponent />
+      {/* <HeadingComponent></HeadingComponent> */}
+      {/* {HeadingComponent()} */}
+      <h1>Namaskar Sahab 2</h1>
+    </div>
+  );
+};
+
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
 
-{
-  /*   ----------- suppose we want this through react
+//this is how we render REACT COMPONENT
+root.render(<HeadingComponent2 />);
 
-<div id="parent">
-  <div id="child">
-    <h1>i m h1 tag</h1>     -----these 2 are siblings
-    <h2>i m h2 tag</h2>     ------both are siblings(for them clearly we can put them inside arrays)
-  </div>
-</div> */
-}
-
-// const pandur = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement("div", { id: "child" }, [
-//     React.createElement("h1", {}, "i m h1 tag"),
-//     React.createElement("h2", {}, "i m h2 tag"),
-//   ])
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(pandur);
+// root.render(jsxHeading);
